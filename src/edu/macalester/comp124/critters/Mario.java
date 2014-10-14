e242c97ae6f458396788eb270badad7b2991d094
bretjackson <bjackson@macalester.edu>
2014-10-14 16:30:33 -0500

    Switched llcoolj to mario

--- src/edu/macalester/comp124/critters/Mario.java
+++ src/edu/macalester/comp124/critters/Mario.java
@@ -7,42 +7,42 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.IOException;
 
-public class Llcoolj extends Critter{
+public class Mario extends Critter{
 	
 	@Override
 	protected void buildGraphics() {
 		
-		GRect leftLeg  = new GRect(35, 200, 35, 150);
+		GRect leftLeg  = new GRect(55, 200, 35, 70);
         leftLeg.setColor(strokeColor);
         leftLeg.setFilled(true);
         leftLeg.setFillColor(strokeColor);
         addLeg(new Leg(leftLeg, 2));
         
-        GRect rightLeg  = new GRect(75, 200, 35, 150);
+        GRect rightLeg  = new GRect(100, 200, 35, 70);
         rightLeg.setColor(strokeColor);
         rightLeg.setFilled(true);
         rightLeg.setFillColor(strokeColor);
         addLeg(new Leg(rightLeg, 2));
         
        
-		GImage LlcoolJ = null;
+		GImage mario = null;
 		try {
-			LlcoolJ = new GImage(ImageIO.read(getClass().getResource("/LLCOOLJ.png")));
+			mario = new GImage(ImageIO.read(getClass().getResource("/Mario.png")));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		
-		getGraphics().add(LlcoolJ);
+		getGraphics().add(mario);
 		
 		
-		addEye(new Eye(10, 0.26, 0.18, Color.RED), 95, 20);
-        addEye(new Eye(10, 0.26, 0.18, Color.RED), 110, 30);
+		addEye(new Eye(10, 0.26, 0.18, Color.RED), 85, 70);
+        addEye(new Eye(10, 0.26, 0.18, Color.RED), 110, 70);
 	}
 	
 	
 	private static final Color		
-		strokeColor = new Color(0, 0, 0),
+		strokeColor = new Color(0, 0, 255),
 		fillColor = new Color(100, 0,0);
 }
