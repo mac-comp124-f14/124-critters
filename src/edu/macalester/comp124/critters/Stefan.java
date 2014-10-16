149416c02c0250c8bf35872c61ed83f15954a1ec
Stefan Krgovic <skrgovic@macalester.edu>
2014-10-16 14:18:56 -0500

    Slash

---
+++ src/edu/macalester/comp124/critters/Stefan.java
@@ -0,0 +1,34 @@
+package edu.macalester.comp124.critters;
+
+import acm.graphics.GImage;
+import acm.graphics.GPolygon;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+
+/**
+ * Created by ${Stefan} on ${10/3/2014}.
+ * This class adds Slash.
+ */
+
+public class Stefan extends Critter {
+
+    @Override
+    protected void buildGraphics() {
+
+
+        GImage slash = null;
+        try {
+            slash = new GImage(ImageIO.read(getClass().getResource("/slash.png")));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        getGraphics().add(slash);
+        addEye(new Eye(10, 0.26, 0.18, Color.GREEN), 75, 89);
+        addEye(new Eye(10, 0.26, 0.18, Color.GREEN), 100, 81);
+
+    }
+
+}
